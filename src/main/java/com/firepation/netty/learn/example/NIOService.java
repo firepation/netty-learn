89ae8062ebda254cf29d546e1c4ecfb9aeba9d3d
firepation <firepation@gmail.com>
2021-05-28 11:08:46 +0800

    netty common example

---
+++ src/main/java/com/firepation/netty/learn/example/NIOService.java
@@ -0,0 +1,88 @@
+package com.firepation.netty.learn.example;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Iterator;
+import java.util.Set;
+
+public class NIOService {
+
+    public static void main(String[] args) throws IOException {
+        Selector serverSelector = Selector.open();
+        Selector clientSelector = Selector.open();
+
+        new Thread(() -> {
+            try {
+                // 对应 IO 编程中服务端启动
+                ServerSocketChannel listenerChannel = ServerSocketChannel.open();
+                listenerChannel.socket().bind(new InetSocketAddress(8000));
+                listenerChannel.configureBlocking(false);
+                listenerChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
+
+                while (true) {
+                    // 检测是否有新的连接, 这里的1指的是阻塞时间为1ms
+                    if (serverSelector.select(1) > 0) {
+                        Set<SelectionKey> set = serverSelector.selectedKeys();
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+
+                        while (keyIterator.hasNext()) {
+                            SelectionKey key = keyIterator.next();
+
+                            if (key.isAcceptable()) {
+                                try {
+                                    // 每来一个新连接, 不需要创建一个线程, 二十直接注册到 clientSelector
+                                    SocketChannel clientChannel = ((ServerSocketChannel) key.channel()).accept();
+                                    clientChannel.configureBlocking(false);
+                                    clientChannel.register(clientSelector, SelectionKey.OP_READ);
+                                } finally {
+                                    keyIterator.remove();
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                while (true) {
+                    // 批量轮询是否有那些连接有数据刻度, 这里的1指的是阻塞时间为1ms
+                    if (clientSelector.select(1) > 0) {
+                        Set<SelectionKey> set = clientSelector.selectedKeys();
+                        Iterator<SelectionKey> keyIterator = set.iterator();
+
+                        while (keyIterator.hasNext()) {
+                            SelectionKey key = keyIterator.next();
+
+                            if (key.isReadable()) {
+                                try {
+                                    SocketChannel clientChannel = (SocketChannel) key.channel();
+                                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                                    // 面向 Buffer
+                                    clientChannel.read(byteBuffer);
+                                    byteBuffer.flip();
+                                    System.out.println(Charset.defaultCharset().newDecoder().decode(byteBuffer)
+                                        .toString());
+                                } finally {
+                                    keyIterator.remove();
+                                    key.interestOps(SelectionKey.OP_READ);
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (IOException ignored) {
+            }
+        }).start();
+    }
+
+}
